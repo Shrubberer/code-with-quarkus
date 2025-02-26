2dfd96a31651604f025fd0f9f42eacc74ceb324b
Shruberrer <swallow.carrying.a.coconut@gmail.com>
2025-02-26 12:41:29 +0200

    added delay option

--- src/main/java/org/acme/GreetingResource.java
+++ src/main/java/org/acme/GreetingResource.java
@@ -13,6 +13,9 @@ import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 
+import javax.ws.rs.QueryParam;
+
+
 
 @Path("/hello")
 public class GreetingResource {
@@ -24,7 +27,16 @@ public class GreetingResource {
     
     @GET
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
+    public String hello(@QueryParam("delay") Long delay) {
+        if (delay != null && delay > 0) {
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                return "Error: Interrupted during sleep";
+            }
+        } 
+        
         LOG.info(sampleEvnVar); 
         return sampleEvnVar;
         
